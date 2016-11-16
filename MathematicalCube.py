ffcfaf6fa06c8eaddd3d69a37a4cc3b4b01d6051
Khushboo Sharma <ksharma161076@gmail.com>
2016-11-16 18:56:59 +0530

    Update MathematicalCube.py

--- MathematicalCube.py
+++ MathematicalCube.py
@@ -1,1 +1,4 @@
-num = int(input('Enter start number='))end = int(input('Enter end number='))for i in range(num,end-1,-1): print(i*i*i), 
+num = int(input('Enter start number='))
+end = int(input('Enter end number='))
+  for i in range(num,end-1,-1): 
+    print(i*i*i), 
